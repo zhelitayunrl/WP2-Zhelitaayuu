e59494043f41a9804ecdb96afcdd16c09ce4d7e7
zhelitayunrl <zhelitaayaunurulliza@gmail.com>
2023-10-02 15:05:39 +0700

    Controllers

---
+++ controllers/Latihan1.php
@@ -0,0 +1,17 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    { 
+        echo "<h1>Biodata</h1>";
+        echo "Nama :Zhelitayu 
+        <p>NIM : 19220493
+        <p>No.Hp : 087712756543
+        <p>Kelas : 19.3A.24
+        <p>Hobi : Olahraga
+        <p>Alamat : Cikarang Selatan, Bekasi
+        <p>Umur : 22tahun";
+
+    }
+}
+?> 
